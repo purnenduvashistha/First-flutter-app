93acb718612653dd98cbeb07f767b1bdad1dc037
Purnendu <mailtopv2299@gmail.com>
2021-05-04 15:22:12 +0530

    First flutter app

---
+++ i_am_rich/android/app/src/main/kotlin/com/purnendu/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.purnendu.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      //home: Center(
+        //child: Text('Hello World'),
+      //),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.jfif'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
